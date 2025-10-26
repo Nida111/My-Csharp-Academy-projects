bcec5e0e23f0ba17691064b7f45ad469ca53093a
nida iqbal <nidaiqbal287@gmail.com>
2025-10-26 21:57:00 +0100

    Math Game completed

--- MathGame/Game.cs
+++ MathGame/Game.cs
@@ -1,13 +1,30 @@
 namespace MathGame;
 
-public class GameOperations
+public class Game
 {
-    public static int GamePoints { get; set; }
-    public  static List<History> history = new List<History>();
-    public static void AdditionGame()
+    public Result PlayGame(string gameName)
+    { 
+        switch (gameName)
+        {
+            case "a":
+                return AdditionGame();
+            case "s" :
+                return SubtractionGame();
+            case "m" :
+                return MultiplicationGame();
+            case "d":
+                return DivisionGame();
+            default:
+                throw new ArgumentException("Unknown Game");
+        }
+
+    }
+    
+    private Result AdditionGame()
         {
             Random random = new Random();
             Console.Clear();
+            int gamePoints = 0;
             Console.WriteLine("Addition game selected");
             for (int i = 0; i < 5; i++)
             {
@@ -21,11 +38,11 @@ public class GameOperations
                     Console.WriteLine("The answer cannot be empty.");
                     userInput = Console.ReadLine();
                 }
-                int validatedInput = UserInterface.InputValidation(userInput);
+                int validatedInput = InputValidation(userInput);
                 if (result == validatedInput)
                 {
                     Console.WriteLine("Good job. The answer is correct. You got 2 points\n");
-                    GamePoints +=2;
+                    gamePoints +=2;
                 }
                 else
                 {
@@ -33,49 +50,56 @@ public class GameOperations
                 }
                 
             }
-            Console.WriteLine($"You got {GamePoints} points");
-            UserInterface.ContinueGameOrNot();
-            history.Add(new History("Addition",GamePoints));
+            Console.WriteLine($"You got {gamePoints} points");
+            Result additionResult = new Result("Addition", gamePoints);
+            return additionResult;
+            
         }
 
-    public static void SubtractionGame()
+    private Result SubtractionGame()
+    {
+        Random random = new Random();
+        Console.Clear();
+        int gamePoints = 0;
+        Console.WriteLine("Subtraction game selected");
+        for (int i = 0; i < 5; i++)
+        {
+            int num1 = random.Next(1, 50);
+            int num2 = random.Next(1, 50);
+            Console.WriteLine($"{num1} - {num2}");
+            int result = num1 - num2;
+            string? userInput = Console.ReadLine();
+            while (userInput == null)
+            {
+                Console.WriteLine("The answer cannot be empty.");
+                userInput = Console.ReadLine();
+            }
+
+            int validatedInput = InputValidation(userInput);
+            if (result == validatedInput)
+            {
+                Console.WriteLine("Good job. The answer is correct. You got 2 points\n");
+                gamePoints += 2;
+            }
+            else
+            {
+                Console.WriteLine("Oh no! The answer is incorrect. You got 0 points\n");
+            }
+
+        }
+
+        Console.WriteLine($"You got {gamePoints} points");
+        Result subtractionResult = new Result("Subtraction", gamePoints);
+        return subtractionResult;
+    }
+
+    private Result MultiplicationGame()
         {
             Random random = new Random();
             Console.Clear();
-            Console.WriteLine("Subtraction game selected");
-            for (int i = 0; i < 5; i++)
-            {
-                int num1 = random.Next(1, 50);
-                int num2 = random.Next(1, 50);
-                Console.WriteLine($"{num1} - {num2}");
-                int result = num1 - num2;
-                string? userInput = Console.ReadLine();
-                while (userInput == null)
-                {
-                    Console.WriteLine("The answer cannot be empty.");
-                    userInput = Console.ReadLine();
-                }
-                int validatedInput = UserInterface.InputValidation(userInput);
-                if (result == validatedInput)
-                {
-                    Console.WriteLine("Good job. The answer is correct. You got 2 points\n");
-                    GamePoints +=2;
-                }
-                else
-                {
-                    Console.WriteLine("Oh no! The answer is incorrect. You got 0 points\n");
-                }
-                
-            }
-            Console.WriteLine($"You got {GamePoints} points");
-            UserInterface.ContinueGameOrNot();
-        }
-
-        public static void MultiplicationGame()
-        {
-            Random random = new Random();
+            int gamePoints = 0;
             Console.WriteLine("Multiplication game selected");
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
                 int num1 = random.Next(1, 20);
                 int num2 = random.Next(1,10);
@@ -87,29 +111,32 @@ public class GameOperations
                     Console.WriteLine("The answer cannot be empty.");
                     userInput = Console.ReadLine();
                 }
-                int validatedInput = UserInterface.InputValidation(userInput);
+                int validatedInput = InputValidation(userInput);
                 if (result == validatedInput)
                 {
                     Console.WriteLine(" Good job. The answer is correct. You got 2 points");
-                    GamePoints +=2;
+                    gamePoints +=2;
                 }
                 else
                 {
                     Console.WriteLine(" oops! The answer is incorrect. You got 0 points");
                 }
             }
-            Console.WriteLine($"You got {GamePoints} points");
-            UserInterface.ContinueGameOrNot();
+            Console.WriteLine($"You got {gamePoints} points");
+            Result multiplyResult = new Result("Multiplication", gamePoints);
+            return multiplyResult;
         }
 
-        public static void  DivisionGame()
+        private Result DivisionGame()
         {
             // The divisions should result on INTEGERS ONLY and dividends should go from 0 to 100
             // . Example: Your app shouldn't present the division 7/2 to the user, since it doesn't result in an integer.
             Random random = new Random();
+            Console.Clear();
+            int gamePoints = 0;
             Console.WriteLine("Division game selected");
             
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
                 int num1;
                 int num2;
@@ -127,20 +154,41 @@ public class GameOperations
                     Console.WriteLine("The answer cannot be empty.");
                     userInput = Console.ReadLine();
                 }
-                int validatedInput = UserInterface.InputValidation(userInput);
+                int validatedInput = InputValidation(userInput);
                 if (result == validatedInput)
                 {
                     Console.WriteLine(" Good job. The answer is correct. You got 2 points");
-                    GamePoints +=2;
+                    gamePoints +=2;
                 }
                 else
                 {
                     Console.WriteLine(" oops! The answer is incorrect. You got 0 points");
                 }
             }
-            Console.WriteLine($"You got {GamePoints} points");
-            UserInterface.ContinueGameOrNot();
+            Console.WriteLine($"You got {gamePoints} points");
+            Result divisionResult = new Result("Division", gamePoints);
+            return divisionResult;
             
         }
+        
+        private  int InputValidation(string input)
+        {
+            input = input.Trim();
+            int validInput = 0;
+            bool success = int.TryParse(input, out int number);
+            if (success)
+            {
+                validInput= number;
+            }
+            else
+            {
+                Console.WriteLine("your answer is in correct. U ot zero points ");
+                    
+            }
+            return validInput;
+        }
 
+
+        
+        
 }
---
+++ MathGame/Result.cs
@@ -0,0 +1,13 @@
+namespace MathGame;
+
+public class Result
+{
+    public string Operation { get; } 
+    public int Points { get; }
+    public Result(string operation, int points)
+    {
+        Operation = operation;
+        Points = points;
+    }
+    
+}
